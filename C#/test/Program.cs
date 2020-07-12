459798f5d786fc35f7fa8fa12a0905890e3ef600
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-07-13 00:34:29 +0200

    test MATRIX complete

--- C#/test/Program.cs
+++ C#/test/Program.cs
@@ -6,7 +6,35 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "MATRIX";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            void PillPrompt()
+            {
+                Console.WriteLine("Red pill or blue pill? [R/B]");
+                string answer = Console.ReadLine();
+
+                if (answer == "R")
+                {
+                    Console.WriteLine("I see. So you have chosen to live in the truth. Come with me. (Press any key to continue)");
+                    return;
+                }
+                else if (answer == "B")
+                {
+                    Console.WriteLine("I understand. The truth can be harsh. Ignorance is bliss after all. You won't see me again");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Don't get sidetracked. Please answer the question.");
+                    PillPrompt();
+                    return;
+                }
+            }
+
+            PillPrompt();
+
             Console.ReadKey();
         }
     }
